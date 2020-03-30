044c0d83ce97cbbc92dbae5b7b6041b5e683af43
BZIvanov <biser.z.ivanov@gmail.com>
2020-03-30 23:48:33 +0300

    more info

---
+++ 02.Intermediate/Practice/Predicates.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* input
+                1 10
+                odd
+            */
+            int[] bounds = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string query = Console.ReadLine();
+
+            // function which accepts parameters and returns boolean is predicate
+            Predicate<int> predicate = query == "odd" ?
+                new Predicate<int>((n) => n % 2 != 0) :
+                new Predicate<int>((n) => n % 2 == 0);
+
+            List<int> result = new List<int>();
+
+            for (int i = bounds[0]; i <= bounds[1]; i++)
+            {
+                if (predicate(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', result));
+        }
+    }
+}
